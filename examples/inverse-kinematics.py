90512452406c0769bbce46a7c63cab2deb319063
JiaheXu <xjh4438318846@gmail.com>
2024-08-25 11:04:55 -0400

    update for viper

--- examples/inverse-kinematics.py
+++ examples/inverse-kinematics.py
@@ -2,22 +2,32 @@ from __future__ import print_function
 
 import numpy as np
 from numpy.linalg import norm, solve
-
+from sys import argv
+from os.path import dirname, join, abspath
 import pinocchio
+import time
 
-model = pinocchio.buildSampleModelManipulator()
+pinocchio_model_dir = join(dirname(dirname(str(abspath(__file__)))), "models")
+
+# You should change here to set up your own URDF file or just pass it as an argument of this example.
+urdf_filename = (
+    "./vx300s.urdf"
+)
+model = pinocchio.buildModelFromUrdf(urdf_filename)
+# model = pinocchio.buildSampleModelManipulator()
 data = model.createData()
 
 JOINT_ID = 6
-oMdes = pinocchio.SE3(np.eye(3), np.array([1.0, 0.0, 1.0]))
+oMdes = pinocchio.SE3(np.eye(3), np.array([0.3, 0.0, 0.2]))
 
 q = pinocchio.neutral(model)
-eps = 1e-4
+eps = 1e-3
 IT_MAX = 1000
 DT = 1e-1
 damp = 1e-12
 
 i = 0
+start = time.time()
 while True:
     pinocchio.forwardKinematics(model, data, q)
     iMd = data.oMi[JOINT_ID].actInv(oMdes)
@@ -35,7 +45,9 @@ while True:
     if not i % 10:
         print("%d: error = %s" % (i, err.T))
     i += 1
+end = time.time()
 
+print("total time: ", end - start)
 if success:
     print("Convergence achieved!")
 else:
